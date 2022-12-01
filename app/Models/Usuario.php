00f72881c80e30e6d818c4dd6c28dc51239c4729
edinando.vieira <edvieira93@gmail.com>
2022-12-01 19:51:49 -0300

    Configurando o Tymon

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model implements AuthenticatableContract, AuthorizableContract
+class Usuario extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable, HasFactory;
 
@@ -30,4 +31,14 @@ class Usuario extends Model implements AuthenticatableContract, AuthorizableCont
     protected $hidden = [
         'password',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
